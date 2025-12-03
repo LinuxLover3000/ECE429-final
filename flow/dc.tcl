6b1a9b81f637209d1cb87ee5052bbd034c49f05e
LinuxLover3000 <charlieyonkura@gmail.com>
2025-12-02 18:26:51 -0600

    Add given ASIC flow files

---
+++ flow/dc.tcl
@@ -0,0 +1,60 @@
+define_design_lib WORK -path ./WORK
+
+set_app_var target_library "./stdcells.db"
+set_app_var link_library "* ./stdcells.db"
+
+########################################################
+# 1. change the verilog file that you want to synthesize
+set my_verilog_files [list accu.v]
+
+# 2. change the toplevel module as needed
+set my_toplevel accu
+
+# 3. change the clock frequency in Mega Hertz 
+set my_clk_freq_MHz 1000
+#######################################################
+set my_clock_pin clk
+set my_input_delay_ns 0.1
+set my_output_delay_ns 0.1
+
+set verilogout_show_unconnected_pins "true"
+set_ultra_optimization true
+set_ultra_optimization -force
+
+analyze -format verilog $my_verilog_files
+elaborate $my_toplevel
+current_design $my_toplevel
+link
+uniquify
+
+set my_period [expr 1000 / $my_clk_freq_MHz]
+
+set find_clock [ find port [list $my_clock_pin] ]
+if {  $find_clock != [list] } {
+   set clk_name $my_clock_pin
+   create_clock -period $my_period $clk_name
+} else {
+   set clk_name vclk
+   create_clock -period $my_period -name $clk_name
+}
+
+set_driving_cell  -lib_cell INVX1  [all_inputs]
+set_input_delay $my_input_delay_ns -clock $clk_name [remove_from_collection [all_inputs] $my_clock_pin]
+set_output_delay $my_output_delay_ns -clock $clk_name [all_outputs]
+
+compile -ungroup_all -map_effort medium
+compile -incremental_mapping -map_effort medium
+check_design
+
+report_constraint -all_violators
+
+set filename [format "%s%s" $my_toplevel "-post-syn.v"]
+write -format verilog -output $filename
+set filename [format "%s%s" $my_toplevel "-post-syn.sdc"]
+write_sdc $filename
+
+redirect timing.rep { report_timing }
+redirect cell.rep { report_cell }
+redirect power.rep { report_power }
+
+quit
---
+++ flow/innovus.tcl
@@ -0,0 +1,71 @@
+set init_mmmc_file "setup-timing.tcl"
+
+#############################################
+# 1. change the synthesized verilog file name
+set init_verilog "accu-post-syn.v"
+# 2. change the top cell name 
+set init_top_cel "accu"
+#############################################
+
+set init_lef_file "rtk-tech.lef stdcells.lef"
+set init_gnd_net "gnd"
+set init_pwr_net "vdd"
+
+init_design
+
+setAnalysisMode -analysisType onChipVariation -cppr both
+
+globalNetConnect vdd -type pgpin -pin vdd -inst * -verbose
+globalNetConnect gnd -type pgpin -pin gnd -inst * -verbose
+
+sroute -nets {vdd gnd}
+
+floorplan -r 1.0 0.7 4.0 4.0 4.0 4.0
+
+addRing -nets {vdd gnd} -width 0.6 -spacing 0.5 \
+            -layer [list top 7 bottom 7 left 6 right 6]
+
+addStripe -nets {gnd vdd} -layer 6 -direction vertical \
+            -width 0.4 -spacing 0.5 -set_to_set_distance 5 -start 0.5
+
+addStripe -nets {gnd vdd} -layer 7 -direction horizontal \
+            -width 0.4 -spacing 0.5 -set_to_set_distance 5 -start 0.5
+
+place_design
+
+assignIoPins -pin *
+
+ccopt_design
+
+setOptMode -holdFixingCells {BUF_X1}
+setOptMode -holdTargetSlack 0.013 -setupTargetSlack 0.044;
+optDesign -postCTS -outDir timingReports -prefix postCTS_hold -hold
+
+routeDesign 
+optDesign -postRoute -outDir timingReports -prefix postRoute_hold -hold
+
+
+setFillerMode -corePrefix FILL -core "FILLCELL_X4 FILLCELL_X2 FILLCELL_X1"
+addFiller
+
+verifyConnectivity
+verify_drc
+
+saveNetlist final.v
+
+streamOut final.gds \
+            -merge "stdcells.gds" \
+            -mapFile "rtk-stream-out.map"
+
+puts "**************************************"
+puts "* Innovus script finished            *"
+puts "*                                    *"
+puts "* Results:                           *"
+puts "* --------                           *"
+puts "* Layout:  final.gds2                *"
+puts "* Netlist: final.v                   *"
+puts "* Timing:  report_timing	           *"
+puts "* Area:    report_area               *"
+puts "* Power:   report_power              *"
+puts "*                                    *"
+puts "**************************************"
